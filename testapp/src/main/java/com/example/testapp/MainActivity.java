adaf49807f28aa93eaa5e3dac5d7df6af803c8d4
sirapob <sintchia@hotmail.com>
2014-01-16 10:51:31 +0700

    test

--- testapp/src/main/java/com/example/testapp/MainActivity.java
+++ testapp/src/main/java/com/example/testapp/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+TextView test1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
